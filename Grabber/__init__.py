2583759f6a931b72f9547338dafc7c6636627498
soham6540 <104189579+soham6540@users.noreply.github.com>
2025-04-29 14:49:11 +0530

    Add files via upload

---
+++ Grabber/__init__.py
@@ -0,0 +1,44 @@
+import logging  
+from pyrogram import Client 
+
+from telegram.ext import Application
+from motor.motor_asyncio import AsyncIOMotorClient
+
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
+    level=logging.INFO,
+)
+
+logging.getLogger("apscheduler").setLevel(logging.ERROR)
+logging.getLogger('httpx').setLevel(logging.WARNING)
+logging.getLogger("pyrate_limiter").setLevel(logging.ERROR)
+LOGGER = logging.getLogger(__name__)
+
+OWNER_ID = '7717913705'
+sudo_users = ["7717913705", "7738654057", "5629555417", "7096345827", "6388703157", "7499006737", "7369449365", "7467514624", "7253335675", "7135053563", "5610811504"]
+GROUP_ID = "-1002528887253"
+SUPPORT_ID = "-1002528887253"
+TOKEN = "7888451649:AAEqfyQpJOpS1pwXekmLS7gRib3vo-uEUb0"
+mongo_url = "mongodb+srv://botmaker9675208:botmaker9675208@cluster0.sc9mq8b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+PHOTO_URL = ["https://files.catbox.moe/2hsawz.jpg"]
+SUPPORT_CHAT = "seal_Your_WH_Group"
+UPDATE_CHAT = "SEAL_UPDATE"
+BOT_USERNAME = "Seal_Your_Waifu_Bot"
+CHARA_CHANNEL_ID = "-1002643258398"
+api_id = "25635673"
+api_hash = "ec69ce8b56c71541499c914fabd08286"
+JOINLOGS = "-1002528887253"
+LEAVELOGS = "-1002528887253"
+
+application = Application.builder().token(TOKEN).build()
+Grabberu = Client("Grabber", api_id, api_hash, bot_token=TOKEN)
+client = AsyncIOMotorClient(mongo_url)
+db = client['Character_catchers']
+collection = db['anime_characterss']
+group_collection = db['total_groups']
+user_totals_collection = db['user_totalssss']
+message_counts_collection = db['message']
+user_collection = db["user_collectionsss"]
+group_user_totals_collection = db['group_user_totals']
+top_global_groups_collection = db['top_global_groupss']
