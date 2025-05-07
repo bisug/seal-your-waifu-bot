5714fec56fc8260856526ce38caff7ad14da8e28
soham6540 <104189579+soham6540@users.noreply.github.com>
2025-05-07 09:18:12 +0530

    Update __init__.py

--- Grabber/__init__.py
+++ Grabber/__init__.py
@@ -16,7 +16,7 @@ logging.getLogger("pyrate_limiter").setLevel(logging.ERROR)
 LOGGER = logging.getLogger(__name__)
 
 OWNER_ID = '6574393060'
-sudo_users = ["7717913705","6574393060"]
+sudo_users = ["7717913705","6574393060","6388703157"]
 GROUP_ID = "-1002528887253"
 SUPPORT_ID = "-1002528887253"
 TOKEN = ""
