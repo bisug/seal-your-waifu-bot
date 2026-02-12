b738b0c9953c9244917ba091bb3703c0a5452a85
bisug <lannsnolo@gmail.com>
2026-02-12 22:08:38 +0545

    minor updates

--- Grabber/database.py
+++ Grabber/database.py
@@ -7,8 +7,7 @@ mongo_url = config.MONGO_URL
 client = AsyncIOMotorClient(mongo_url)
 db = client['Character_catchers']
 
-from umongo.frameworks.motor_asyncio import MotorAsyncIOInstance
-instance = MotorAsyncIOInstance(db)
+
 
 # Collections
 collection = db['anime_characterss']
--- Grabber/models.py
+++ Grabber/models.py
@@ -1,32 +1,23 @@
-from umongo import Document, fields, EmbeddedDocument
-from Grabber.database import instance
+from pydantic import BaseModel, Field
+from typing import List, Optional
 
-@instance.register
-class Character(Document):
-    id = fields.StringField(required=True, unique=True)
-    name = fields.StringField(required=True)
-    anime = fields.StringField(required=True)
-    rarity = fields.StringField(required=True)
-    img_url = fields.StringField(required=True)
-    zenith_price = fields.IntegerField(default=5)
-    sold_count = fields.IntegerField(default=0)
+class Character(BaseModel):
+    id: str
+    name: str
+    anime: str
+    rarity: str
+    img_url: str
+    zenith_price: int = 5
+    sold_count: int = 0
 
-    class Meta:
-        collection_name = "anime_characterss"
+class UserCharacter(BaseModel):
+    id: str
+    name: str
+    anime: str
+    rarity: str
+    img_url: str
 
-@instance.register
-class UserCharacter(EmbeddedDocument):
-    id = fields.StringField(required=True)
-    name = fields.StringField(required=True)
-    anime = fields.StringField(required=True)
-    rarity = fields.StringField(required=True)
-    img_url = fields.StringField(required=True)
-
-@instance.register
-class User(Document):
-    id = fields.IntegerField(required=True, unique=True)
-    zenith = fields.IntegerField(default=0)
-    characters = fields.ListField(fields.EmbeddedField(UserCharacter), default=[])
-
-    class Meta:
-        collection_name = "user_collectionsss"
+class User(BaseModel):
+    id: int
+    zenith: int = 0
+    characters: List[UserCharacter] = Field(default_factory=list)
