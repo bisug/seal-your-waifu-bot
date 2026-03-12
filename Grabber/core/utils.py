63090b1724f6173a392d799ed7d4c221581b8dc3
bisug <lannsnolo@gmail.com>
2026-03-12 19:45:26 +0545

    yeaaa

--- Grabber/core/utils.py
+++ Grabber/core/utils.py
@@ -14,3 +14,19 @@ def md_escape(text: str) -> str:
     # Characters to escape in MarkdownV2: _ * [ ] ( ) ~ ` > # + - = | { } . !
     escape_chars = r"_*[]()~`>#+-=|{}.!"
     return re.sub(f"([{re.escape(escape_chars)}])", r"\\\1", text)
+
+async def check_member_requirement(chat, min_count=50):
+    """
+    Checks if a chat meets the minimum member requirement for GameBot.
+    Returns (bool, current_count).
+    """
+    from pyrogram import enums
+    if chat.type in [enums.ChatType.PRIVATE, enums.ChatType.BOT]:
+        return True, 0
+        
+    try:
+        count = await chat.get_members_count()
+        return count >= min_count, count
+    except Exception:
+        # If we can't check (e.g., bot not in group), assume True to avoid blocking if the bot just joined
+        return True, 0
