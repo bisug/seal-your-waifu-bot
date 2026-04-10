9e978e2f3a61fc4281251fe3d7c30f44ebb298c8
bisug <lannsnolo@gmail.com>
2026-04-11 00:23:11 +0545

    feat: initialize static frontend assets and Tailwind CSS configuration

---
+++ Grabber/static/assets/search-Cw1K43hr.js
@@ -0,0 +1,1 @@
+import{g as e}from"./UI-CbspiYGd.js";var t=e(`compass`,[[`circle`,{cx:`12`,cy:`12`,r:`10`,key:`1mglay`}],[`path`,{d:`m16.24 7.76-1.804 5.411a2 2 0 0 1-1.265 1.265L7.76 16.24l1.804-5.411a2 2 0 0 1 1.265-1.265z`,key:`9ktpf1`}]]),n=e(`search`,[[`path`,{d:`m21 21-4.34-4.34`,key:`14j7rj`}],[`circle`,{cx:`11`,cy:`11`,r:`8`,key:`4ej97u`}]]);export{t as n,n as t};
