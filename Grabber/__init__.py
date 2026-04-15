fbfaf9d629af172441ceed2d2eb0d7930f570f33
bisug <lannsnolo@gmail.com>
2026-04-15 12:27:02 +0545

    feat: update scraper

--- Grabber/__init__.py
+++ Grabber/__init__.py
@@ -13,7 +13,7 @@ from Grabber.database import (client, collection, db,
                               gamebot_enabled_groups_collection,
                               group_collection, group_user_totals_collection,
                               message_counts_collection,
-                              quiz_questions_collection, sessions_collection,
+                              scraped_characters_collection, sessions_collection,
                               spawns_collection, sudo_collection,
                               total_pm_users, user_collection,
                               user_totals_collection)
